343944d888a2febe16ee64e50b1a26abd5e597a8
chenyiliang <chenyiliangconan@qq.com>
2015-09-11 16:50:07 +0800

    爬虫bug修改

--- src/main/java/com/github/cyl/crawler/CPICrawler.java
+++ src/main/java/com/github/cyl/crawler/CPICrawler.java
@@ -23,7 +23,10 @@ public class CPICrawler {
 
 	private static final Pattern YEAR_MONTH_EX = Pattern.compile("(\\d{4})年(\\d{1,2})月");
 	private static final Pattern UN_CN_CHAR = Pattern.compile("[^\u4e00-\u9fa5]");
+	private static final String START_MARK_WORD = "居民消费价格";
 	private static final List<String> STOP_WORDS = Arrays.asList("分类别");
+	private static final List<String> BEGIN_REMOVE_WORDS = Arrays.asList("其中", "一", "二", "三", "四", "五", "六", "七", "八",
+			"九", "十");
 
 	public CPICrawler(String urlsPath, String charsetName) {
 		String line = null;
@@ -48,17 +51,6 @@ public class CPICrawler {
 		this.urlList = urlList;
 	}
 
-	// public void crawlCpiToMongoDB() {
-	// for (int i = 0; i < urlList.size(); i++) {
-	// Document doc = null;
-	// try {
-	// doc = Jsoup.connect(urlList.get(i)).get();
-	// } catch (IOException e) {
-	// throw new RuntimeException("爬取网址连接失败！", e);
-	// }
-	// }
-	// }
-
 	public Map<String, Object> parseCPIDoc(Document doc) {
 		Map<String, Object> map = new LinkedHashMap<String, Object>();
 
@@ -82,7 +74,8 @@ public class CPICrawler {
 				trs = trs2;
 			}
 		}
-		outter: for (int i = 3; i < trs.size(); i++) {
+
+		outter: for (int i = 2; i < trs.size(); i++) {
 			Element tr = trs.get(i);
 			Elements tds = tr.getElementsByTag("td");
 
@@ -107,7 +100,7 @@ public class CPICrawler {
 				if (j == 0) {
 					key = UN_CN_CHAR.matcher(cell).replaceAll("");
 					// System.out.println(key);
-					if (STOP_WORDS.contains(key)) {
+					if (STOP_WORDS.contains(key) || (i == 2 && !key.equals(START_MARK_WORD))) {
 						continue outter;
 					}
 				} else {
@@ -115,9 +108,19 @@ public class CPICrawler {
 					indicator.add(Float.valueOf(cell));
 				}
 			}
-			map.put(key, indicator);
+			map.put(processKey(key), indicator);
 		}
 		return map;
+	}
+
+	private String processKey(String key) {
+		for (int i = 0; i < BEGIN_REMOVE_WORDS.size(); i++) {
+			String beginRemoveWord = BEGIN_REMOVE_WORDS.get(i);
+			if (key.startsWith(beginRemoveWord)) {
+				return key.replaceFirst(beginRemoveWord, "");
+			}
+		}
+		return key;
 	}
 
 	private int[] parseYearMonthFromTitle(String title) {
--- src/test/java/com/github/cyl/crawler/CPICrawlerTest.java
+++ src/test/java/com/github/cyl/crawler/CPICrawlerTest.java
@@ -13,7 +13,8 @@ public class CPICrawlerTest {
 	@Test
 	public void test() throws IOException {
 		CPICrawler crawler = new CPICrawler("D:/yicai_workspace/2014CPI.txt", "utf-8");
-		Document document = Jsoup.connect("http://www.stats.gov.cn/tjsj/zxfb/201509/t20150910_1242519.html").get();
+		Document document = Jsoup.connect("http://www.stats.gov.cn/tjsj/zxfb/201509/t20150910_1242519.html")
+				.header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0").get();
 		Map<String, Object> map = crawler.parseCPIDoc(document);
 		Set<Entry<String, Object>> set = map.entrySet();
 		for (Entry<String, Object> entry : set) {
