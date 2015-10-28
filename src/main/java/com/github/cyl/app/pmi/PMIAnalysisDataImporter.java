abb0ac097e701ff9a09b381e200140009edef434
chenyiliang <chenyiliangconan@qq.com>
2015-10-28 13:47:36 +0800

    完成pmi数据手动爬取

--- src/main/java/com/github/cyl/app/pmi/PMIAnalysisDataImporter.java
+++ src/main/java/com/github/cyl/app/pmi/PMIAnalysisDataImporter.java
@@ -5,7 +5,7 @@ import com.github.cyl.service.pmi.PMIAnalysisService;
 public class PMIAnalysisDataImporter {
 
 	public static void main(String[] args) {
-		new PMIAnalysisService().fetchPMIAnalysisDataToMongo("", "utf-8");
+		new PMIAnalysisService().fetchPMIAnalysisDataToMongo("D:/data/pmiAnalysisUrl.txt", "utf-8");
 	}
 
 }
--- src/main/java/com/github/cyl/app/pmi/PMIWuliuDataImporter.java
+++ src/main/java/com/github/cyl/app/pmi/PMIWuliuDataImporter.java
@@ -5,7 +5,7 @@ import com.github.cyl.service.pmi.PMIWuliuService;
 public class PMIWuliuDataImporter {
 
 	public static void main(String[] args) {
-		new PMIWuliuService().fetchPMIWuliuDataToMongo("", "utf-8");
+		new PMIWuliuService().fetchPMIWuliuDataToMongo("D:/data/pmiWuliuUrl.txt", "utf-8");
 	}
 
 }
